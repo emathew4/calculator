7139aabc01692ab288b4a4d20b76b7996aac7d2f
emathew4 <emathew4@gwc185mach11.care.asu.edu>
2017-09-12 15:51:29 -0700

    Added answer Display

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var answerDisplay: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
