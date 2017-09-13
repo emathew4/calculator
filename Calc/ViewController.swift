61283887ebedad47406b86eeb10922b71b4be5ae
emathew4 <emathew4@gwc185mach11.care.asu.edu>
2017-09-12 17:31:03 -0700

    Displays when numbers are tapped

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,21 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    var isAlreadyTyping = false
+    var nums: [Float] = []
+    var opers: [String] = []
+    
     @IBOutlet weak var answerDisplay: UILabel!
     @IBOutlet weak var equationDisplay: UILabel!
     
     @IBAction func tapNum(_ sender: UIButton) {
+        if isAlreadyTyping{
+            equationDisplay.text = equationDisplay.text! + sender.currentTitle!
+        } else {
+            isAlreadyTyping = true
+            equationDisplay.text = sender.currentTitle
+        }
+        
     }
     @IBAction func tapOperation(_ sender: UIButton) {
     }
     @IBAction func tapClear(_ sender: UIButton) {
         equationDisplay.text = "0"
+        isAlreadyTyping = false
     }
     @IBAction func tapSwitchSign(_ sender: UIButton) {
     }
